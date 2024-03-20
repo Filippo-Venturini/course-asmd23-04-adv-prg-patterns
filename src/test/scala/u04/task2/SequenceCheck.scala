580ac1e9c1e4c0fa43c74f046d71bb9b4c2b3f25
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-03-20 14:37:40 +0100

    Implemented Sequence ADT

--- src/test/scala/u04/task2/SequenceCheck.scala
+++ src/test/scala/u04/task2/SequenceCheck.scala
@@ -1,7 +1,20 @@
 package scala.u04.task2
 
-import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+import org.scalacheck.{Arbitrary, Gen, Properties}
 
 object SequenceCheck extends Properties("Sequence"):
-  ???
+
+  import scala.u04.task2.Sequences.BasicSequenceADT.*
+  def sequenceGen(): Gen[Sequence[Int]] = for
+    size <- Gen.choose(0, 100) // Use the Gen.choose method to determine the size of the sequence
+    elements <- Gen.listOfN(size, Gen.choose(0, 100)) // Generate a list of random integers
+  yield elements.foldRight[Sequence[Int]](nil())((e, acc) => cons(e, acc)) // Convert the list to a sequence
+
+  //Pick a random mapper between the three
+  val mapperArbitrary: Arbitrary[Int => Int] = Arbitrary(Gen.oneOf[Int => Int](_ + 1, _ * 2, x => x * x))
+
+ /* property("map axiom") =
+    forAll(sequenceGen(), mapperArbitrary.arbitrary): ???*/
+
 
