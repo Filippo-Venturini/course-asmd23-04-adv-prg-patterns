f7ca1e4ebace3c53ef14587007c2ceb824c85ff0
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-04-03 19:40:54 +0200

    Tested option monad

--- src/test/scala/u04/monads/MonadsCheck.scala
+++ src/test/scala/u04/monads/MonadsCheck.scala
@@ -1,19 +1,34 @@
 package scala.u04.monads
 
 import u04.monads.Optionals
-import org.scalacheck.Properties
-import u04.monads.Monads.Monad
+import org.scalacheck.{Gen, Properties}
 
 import scala.u04.monads.SequenceCheck.property
+import Optionals.Optional
+import Optionals.Optional.{Empty, Just}
+import org.scalacheck.Prop.forAll
 
 object MonadsCheck extends Properties("Monads"):
 
   import Optionals.{*, given} // importing also given terms
   import Optional.* // importing Optional algorithms
 
-  val opt: Optional[Double] = Just(java.lang.Math.random()).filter(_ < 0.9)
-  val f: Double => Optional[Double] = x => Just(x/2)
-  //val optionalValue: Optionals.Optional[Int] = Monad[Optionals.Optional].unit(someValue)
-/*
+  def doubleGen(): Gen[Double] = Gen.choose(0, 100)
+  def fGen(): Gen[Double => Optional[Double]] = Gen.oneOf[Double => Optional[Double]](x => Just(x + 1), x => Just(x * 2), x => Just(x * x))
+
+  // monadic laws :
+  // Left identity : flatMap ( unit (a),f) === f(a)
+  // Right identity : flatMap (m, unit (_)) === m
+  // Associativity : flatMap ( flatMap (m,f),g) === flatMap (m,x=>flatMap (f(x),g))
+  
+  property("Left identity")=
+    forAll(doubleGen(), fGen()): (a, f) =>
+      Just(a).flatMap(f) == f(a)
+
   property("Right identity")=
-    Monad[Optional].unit(someValue)*/
+    forAll(doubleGen()): a =>
+      Just(a).flatMap(x => Just(x)) == Just(a)
+
+  property("Associativity")=
+    forAll(doubleGen(), fGen(), fGen()): (a, f, g) =>
+      Just(a).flatMap(f).flatMap(g) == Just(a).flatMap(x => f(x).flatMap(g))
